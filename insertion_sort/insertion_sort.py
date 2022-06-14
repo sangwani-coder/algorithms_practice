72ead4c82fdd590d65db0305c96b4fb4fc126ac5
sangwani-coder <mightypz2@gmail.com>
2022-06-14 20:04:36 +0100

    Implement insertion_sort descending order

--- insertion_sort/insertion_sort.py
+++ insertion_sort/insertion_sort.py
@@ -6,7 +6,7 @@
 """
 
 
-def insertion_sort(arr=[]):
+def insertion_sort_asc(arr=[]):
 	"""Sorts an unsorted list in nondescending order
 		using the INSERTION SORT algorithm
 		param: Unsorted list of numbers
@@ -26,6 +26,26 @@ def insertion_sort(arr=[]):
 			#print(arr)
 	return (arr)
 
+def insertion_sort_desc(arr=[]):
+	"""Sorts an unsorted list in nonincreasing order
+		using the INSERTION SORT algorithm
+		param: Unsorted list of numbers
+		return: Sorted list of numbers non-descending
+	"""
+	for j in range(1, len(arr)):
+		key = arr[j]
+		# Insert A[j] into the sorted sequence
+		# A[0.....j]
+		i = j - 1
+		while i >= 0 and arr[i] < key:
+			arr[i + 1] = arr[i]
+			i = i - 1
+			# place key after the element just smaller
+			# than it
+			arr[i + 1] = key 
+			#print(arr)
+	return (arr)
+
 
 if __name__ == '__main__':
 	""" Defines unsorted list
@@ -33,15 +53,11 @@ if __name__ == '__main__':
 		and print sorted lists
 	"""
 	print("Running insertion sort...")
-	A = [5, 2, 4, 6, 1, 3]
-	B = [31, 41, 59, 26, 41, 58]
-	C = [391, 441, 59, 236, 41, 58, 776]
-
+	A = [5, 2, 4, 6, 1, 3, 5, 7, 9, 10, 0]
+	
 	print('Unsorted Array A:', A)
-	print('Unsorted Array B:', B)
-	print('Unsorted Array B:', C)
-	print("----------------------------------")
-	print('Sorted Array A', insertion_sort(A))
-	print('Sorted Array B:', insertion_sort(B))
-	print('Sorted Array B:', insertion_sort(C))
+	print("-------------------------------------------")
+	print('Sorted Array Ascending ', insertion_sort_asc(A))
+	print("-------------------------------------------")
+	print('Sorted Array Descending A:', insertion_sort_desc(A))
 
